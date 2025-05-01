61729d376df26d8da7c5a83e45c5d8b1407b069b
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-05-01 20:58:14 +0700

    mapping struct demo

--- contracts/mapping_struct.sol
+++ contracts/mapping_struct.sol
@@ -2,15 +2,34 @@ pragma solidity ^0.8.0;
 
 contract MappingStruct {
 
-    mapping (address => uint ) public  balanceReceived;
+    mapping (address => Balance ) public balanceReceived;
+    // when use this you can balanceReceived[totalBlance] 
+    
 
+    struct Payment {
+        uint amount;
+        uint timestamp;
+    }
+
+    struct Balance {
+        uint totalBalance;
+        uint numPayments;
+
+        mapping (uint => Payment) payments;
+    }
 
     function getBalance() public view returns (uint){
         return address(this).balance; // get blance current account received money
     }
 
     function sendMoney() public  payable {
-        balanceReceived[msg.sender] += msg.value; // who are send money
+
+        balanceReceived[msg.sender].totalBalance  += msg.value;
+
+        Payment memory payment = Payment(msg.value, block.timestamp); // use memory when function end it remove 
+
+        balanceReceived[msg.sender].payments[balanceReceived[msg.sender].numPayments] = payment;
+        balanceReceived[msg.sender].numPayments++;
     }
 
     function withdrawMoneyto(address payable _to) public {
@@ -19,11 +38,11 @@ contract MappingStruct {
 
 
   function withdrawMoney(address payable _to, uint _amount) public {
-        require(balanceReceived[msg.sender] >= _amount, " not enough funds"); // check balance in account > amount input?
+        require(balanceReceived[msg.sender].totalBalance >= _amount, " not enough funds"); // check balance in account > amount input?
     
-    balanceReceived[msg.sender] -= _amount; // - balance current account example 2000000000000000000 (current balance) when send 1000000000000000000 else 1000000000000000000
+        balanceReceived[msg.sender].totalBalance -= _amount; // - balance current account example 2000000000000000000 (current balance) when send 1000000000000000000 else 1000000000000000000
 
-    _to.transfer(_amount); // send money to address with _amount input 
+        _to.transfer(_amount); // send money to address with _amount input 
     }
 
     
